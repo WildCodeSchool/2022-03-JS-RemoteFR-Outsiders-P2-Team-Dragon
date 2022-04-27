f40b75effefd1b92601c2c45247cd0f65d9a3b71
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-04-27 15:42:24 +0200

    connexion favoriteButton with BackLogOffer in Suivi

--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -17,34 +17,20 @@ function Accueil() {
     // });
   };
   return (
-    <>
-      <div>
-        <Search />
-        {offerTemplate.map((offer) => {
-          return (
-            // Partie Oscar : ici on envoie les props qui viennent d'App et les transmets vers JobOffer...
-            <JobOffer
-              key={offer.id}
-              handleLiked={handleLiked}
-              showFavoriteButton
-              offer={offer}
-            />
-          );
-        })}
-      </div>
-      <div>
-        <h1>Suivi de Candidature</h1>
-        <div>
-          {offerTemplate.map((offer) =>
-            offer.isFavorite ? (
-              <JobOffer showFavoriteButton={false} offer={offer} />
-            ) : (
-              ""
-            )
-          )}
-        </div>
-      </div>
-    </>
+    <div>
+      <Search />
+      {offerTemplate.map((offer) => {
+        return (
+          // Partie Oscar : ici on envoie les props qui viennent d'App et les transmets vers JobOffer...
+          <JobOffer
+            key={offer.id}
+            handleLiked={handleLiked}
+            showFavoriteButton
+            offer={offer}
+          />
+        );
+      })}
+    </div>
   );
 }
 
