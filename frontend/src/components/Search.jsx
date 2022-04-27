5962959d45555b5ef9c2db193a111bc6c8865ff9
Oscar AGUDELO <oscar.agudelo.pro@gmail.com>
2022-04-27 11:37:08 +0200

    problem heart ok

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -15,7 +15,6 @@ export default function Search() {
     event.preventDefault();
     console.warn(inputs);
   };
-  
   return (
     <div className="searchjob">
       <form className="formjob" onSubmit={handleSubmit}>
