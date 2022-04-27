5b4093865b55c19e4be64973856d0dc4e929023f
Oscar AGUDELO <oscar.agudelo.pro@gmail.com>
2022-04-27 11:14:46 +0200

    commir pourr aller vers template

--- frontend/src/components/FavoriteButton.jsx
+++ frontend/src/components/FavoriteButton.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function FavoriteButton({ offer, handleLiked }) {
   return (
-    <div className="addFavorite">
+    <div>
       <button
         className="buttonAddFavorite"
         type="button"
