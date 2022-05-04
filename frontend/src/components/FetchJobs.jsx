521b38f3547fda2c98eb1be0b7da2fb4bdc6aa04
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-05-04 10:19:02 +0200

    checkout sur US_Subnavbar

--- frontend/src/components/FetchJobs.jsx
+++ frontend/src/components/FetchJobs.jsx
@@ -18,7 +18,7 @@ export default function FetchJobs() {
       .then((response) => response.data)
       .then((data) => {
         console.warn(data);
-        setStateJob(statejob);
+        setStateJob(data.statejob);
       });
   };
 
