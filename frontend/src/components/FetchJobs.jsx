7f352605bc08cd862644afb87cef779ff9926174
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-05-04 17:44:30 +0200

    backend et frontend

--- frontend/src/components/FetchJobs.jsx
+++ frontend/src/components/FetchJobs.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function FetchJobs() {
   const [statejob, setStateJob] = React.useState("Hello");
   const API =
-    "https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/3136965";
+    "https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search?motsCles=reactjs";
   // 👎 const API = "https://candidat.pole-emploi.fr/offres/recherche/detail/3136965";
   /* const token = user.data.id; take only token and save in token variable */
 
