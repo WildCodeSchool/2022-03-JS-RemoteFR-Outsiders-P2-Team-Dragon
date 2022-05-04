124750d1c18e3efe8065b803e21bddcf6a3ac33a
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-05-04 12:42:28 +0200

    soucis de liens

--- frontend/src/components/SubNavbar.jsx
+++ frontend/src/components/SubNavbar.jsx
@@ -1,25 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import JaiPostule from "./JaiPostule";
+import Action from "./Action";
+import Feedback from "./Feedback";
+import JeVaisPostuler from "./JeVaisPostuler";
 import "../assets/SubNavbar.css";
 
 function SubNavbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/je-vais-postuler">Je vais postuler</Link>
-        </li>
-        <li>
-          <Link to="/j-ai-postule/">J&apos;ai Postulé</Link>
-        </li>
-        <li>
-          <Link to="/action">Action</Link>
-        </li>
-        <li>
-          <Link to="/feedback">Feedback</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/je-vais-postuler">Je vais postuler</Link>
+          </li>
+          <li>
+            <Link to="/j-ai-postule/">J&apos;ai Postulé</Link>
+          </li>
+          <li>
+            <Link to="/action">Action</Link>
+          </li>
+          <li>
+            <Link to="/feedback">Feedback</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/suivi/je-vais-postuler" element={<JeVaisPostuler />} />
+        <Route path="/j-ai-postule" element={<JaiPostule />} />
+        <Route path="/action" element={<Action />} />
+        <Route path="/feedback" element={<Feedback />} />
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/pages/Suivi.jsx
+++ frontend/src/pages/Suivi.jsx
@@ -1,23 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import SubNavbar from "@components/SubNavbar";
-import JeVaisPostuler from "@components/JeVaisPostuler";
-import JaiPostule from "@components/JaiPostule";
-import Action from "@components/Action";
-import Feedback from "@components/Feedback";
 
 function Suivi() {
-  return (
-    <>
-      <SubNavbar />
-      <Routes>
-        <Route path="/je-vais-postuler" element={<JeVaisPostuler />} />
-        <Route path="/j-ai-postule" element={<JaiPostule />} />
-        <Route path="/action" element={<Action />} />
-        <Route path="/feedback" element={<Feedback />} />
-      </Routes>
-    </>
-  );
+  return <SubNavbar />;
 }
 
 export default Suivi;
