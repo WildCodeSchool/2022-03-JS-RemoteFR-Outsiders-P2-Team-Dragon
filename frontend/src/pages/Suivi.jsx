41b0bf74ad49d69afbcdc515f6f5336555dd5da0
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-05-05 12:09:23 +0200

    US_SubNavbar presque au point

--- frontend/src/pages/Suivi.jsx
+++ frontend/src/pages/Suivi.jsx
@@ -1,30 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "@assets/Common.css";
 
-// import SubNavbar from "@components/SubNavbar";
-// function Suivi() {
-// return <SubNavbar />;
-
 function Suivi() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/suivi/jevaispostuler">Je vais postuler</Link>
-        </li>
-        <li>
-          <Link to="/suivi/jaipostule">J&apos;ai Postulé</Link>
-        </li>
-        <li>
-          <Link to="/suivi/action">Action</Link>
-        </li>
-        <li>
-          <Link to="/suivi/feedback">Feedback</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+  return <div />;
 }
 
 export default Suivi;
