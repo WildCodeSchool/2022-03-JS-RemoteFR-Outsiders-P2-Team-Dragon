02ae98a9169581708bea836c1a1678d4d9e847fe
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-05-10 11:30:08 +0200

    ajout filters

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -7,7 +7,7 @@ import "@assets/Search.css";
 export default function Search({ setJobs }) {
   const [inputs, setInputs] = useState({});
   const [filters, setFilters] = useState([]);
-  const API = `https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search?motsCles=${inputs.job}&typeContrat=CDD,CDI`;
+  const API = `https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search?motsCles=${inputs.job}&typeContrat=${filters}`;
 
   const config = {
     headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
