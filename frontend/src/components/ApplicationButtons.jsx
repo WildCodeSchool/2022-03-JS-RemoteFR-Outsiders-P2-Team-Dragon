0e65743f551c941958aae1e77b21f1d63b5a92d6
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-05-11 11:26:53 +0200

    profil css en cours

--- frontend/src/components/ApplicationButtons.jsx
+++ frontend/src/components/ApplicationButtons.jsx
@@ -11,7 +11,7 @@ export default function AplicationButtons({
     handlePostuled(offer);
     handleResponsed(offer);
     // eslint-disable-next-line no-alert
-    alert("N'oublies pas écrire un commentaire");
+    alert("N'oublies pas d'écrire un commentaire !");
   }
   function buttonsApplications2() {
     handlePostuled(offer);
--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import "../assets/Common.css";
+import "../assets/Profil.css";
 
 function Profil() {
-  return <div>Profil</div>;
+  return (
+    <div className="profilcontainer">
+      <div className="identity">
+        <img src="..\src\assets\moi.jpg" alt="profil" />
+        <h2>Nom :</h2>
+        <p>Boinet</p>
+        <h2>Prénom : </h2>
+        <p>Christophe</p>
+      </div>
+      <div className="goals">
+        <h1>Mes Objectifs professionnels</h1>
+        <ul>
+          <li>
+            Job :<p>Développeur FullStack</p>
+          </li>
+          <li>
+            Localisation :<p>Région PACA</p>
+          </li>
+          <li>
+            Salaire :<p>35000€</p>
+          </li>
+          <li>
+            Contrat :<p>CDI</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Profil;
